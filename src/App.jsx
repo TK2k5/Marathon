c3879c54e6754a4c8971c9f4e5c18241e58c5d75
Trieu <nhtrieu2k5@gmail.com>
2024-01-10 09:38:15 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="card">
+        <h1 className="text-cyan-500 text-lg ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
+          itaque!
+        </h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
