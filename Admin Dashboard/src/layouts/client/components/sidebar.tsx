8e526ed7d980dab5a4d20b1b5ac5d24ff16f3e02
Triều <nhtrieu2k5@gmail.com>
2024-02-23 22:10:28 +0700

    sidebar

--- Admin Dashboard/src/layouts/client/components/sidebar.tsx
+++ Admin Dashboard/src/layouts/client/components/sidebar.tsx
@@ -1,3 +1,30 @@
+import { NavigateBar } from '.'
+import { initialData } from '..'
+import { useState } from 'react'
+
 export const SidebarLayout = () => {
-  return <div>SidebarLayout</div>
+  const [value, setValue] = useState<number>(0)
+
+  const handleClick = (index: number) => {
+    setValue(index)
+  }
+  return (
+    <div className='mt-5'>
+      <div className='w-[350px] h-screen pl-10 pr-5 border-r border-r-gray-l9'>
+        <div className='border border-black rounded-[5px] px-2 py-3'>
+          {initialData.map((data, index) => (
+            <NavigateBar
+              handleClick={handleClick}
+              id={value}
+              index={index}
+              name={data.name}
+              title={data.title}
+              url={data.url}
+              icon={data.icons}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
---
+++ Admin Dashboard/src/layouts/client/init.tsx
@@ -0,0 +1,25 @@
+import { Dashboard, ExcutiveIcon, UserIcon } from '@/components'
+
+export const initialData = [
+  {
+    id: 0,
+    name: 'Dashboard',
+    title: 'Main',
+    icons: <Dashboard />,
+    url: ''
+  },
+  {
+    id: 1,
+    name: 'Admin',
+    title: 'User Details',
+    icons: <UserIcon />,
+    url: ''
+  },
+  {
+    id: 2,
+    name: 'Excutives',
+    title: 'User Details',
+    icons: <ExcutiveIcon />,
+    url: ''
+  }
+]
